301cdd6361cd2dcd996cf24b0eabf8cc013a6024
Ala a <allan2021jose@gmail.com>
2025-04-30 02:42:47 -0300

    Adicionado better_coal

---
+++ src/main/java/com/ala/betterfurnacesmod/BetterFurnacesMod.java
@@ -0,0 +1,18 @@
+package com.ala.betterfurnacesmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BetterFurnacesMod implements ModInitializer {
+	public static final String MOD_ID = "betterfurnacesmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModItems.initialize();
+
+		LOGGER.info("Better Furnaces Mod is initializing...");
+	}
+}
---
+++ src/main/java/com/ala/betterfurnacesmod/ModItems.java
@@ -0,0 +1,39 @@
+package com.ala.betterfurnacesmod;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.registry.FuelRegistryEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
+
+public class ModItems {
+    public static final Item BETTER_COAL = register("better_coal", Item::new, new Item.Settings());
+
+
+    public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+
+        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(BetterFurnacesMod.MOD_ID, name));
+
+        Item item = itemFactory.apply(settings.registryKey(itemKey));
+
+        Registry.register(Registries.ITEM, itemKey, item);
+
+        return item;
+    }
+
+    public static void initialize() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register((itemGroup) -> itemGroup.add(ModItems.BETTER_COAL));
+
+        FuelRegistryEvents.BUILD.register((builder, context) -> {
+            builder.add(ModItems.BETTER_COAL, 30 * 20);
+        });
+
+    }
+
+}
